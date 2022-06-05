c5c12e00ea214a1e3234de105a0ec6744a383fa4
Kelly Narine <knarine12@hotmail.ca>
2022-06-04 23:12:40 -0400

    Employee Add/Edit Function in Progress

--- employee-management-ui/src/app/employee/add-edit-emp/add-edit-emp.component.ts
+++ employee-management-ui/src/app/employee/add-edit-emp/add-edit-emp.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-edit-emp',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditEmpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:SharedService) { }
+  @Input() emp:any;
+  EmployeeId:string = "";
+  EmployeeName:string = "";
+  Department:string = "";
+  DateOfJoining:string = "";
+  PhotoFileName:string = "";
+  PhotoFilePath:string = "";
+
+  DepartmentsList:any=[]; // for department list dropdown
 
   ngOnInit(): void {
+    this.loadDepartmentList();
+  }
+
+  loadDepartmentList() {
+    this.service.getAllDepartmentNames().subscribe((data:any)=>{
+      this.DepartmentsList = data;
+      this.EmployeeId = this.emp.EmployeeId;
+      this.EmployeeName = this.emp.EmployeeName;
+      this.Department = this.emp.Department;
+      this.DateOfJoining = this.emp.DateOfJoining;
+      this.PhotoFileName = this.emp.PhotoFileName;
+      this.PhotoFilePath = this.service.PhotoUrl + this.PhotoFileName;
+    });
+  }
+
+  addDepartment() {
+    var val = {DepartmentId: this.DepartmentId,
+               DepartmentName: this.DepartmentName};
+    this.service.addDepartment(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
+
+  updateDepartment() {
+    var val = {DepartmentId: this.DepartmentId,
+               DepartmentName: this.DepartmentName};
+    this.service.updateDepartment(val).subscribe(res=>{
+      alert(res.toString());
+    });
   }
 
 }
+
